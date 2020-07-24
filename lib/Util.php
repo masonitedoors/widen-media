5ffc070095af5bc72436c4c35c750b1d7b4d30ab
Joseph Fusco <jfusco@masonite.com>
2020-07-24 17:04:06 -0400

    Add new utility methods

--- lib/Util.php
+++ lib/Util.php
@@ -82,4 +82,38 @@ class Util {
 		return $decoded_data;
 	}
 
+	/**
+	 * Sanitize our image URLs:
+	 *
+	 * Update .tif images to be .png.
+	 * Remove query strings.
+	 *
+	 * @param string $image_url The image URL to sanitize.
+	 */
+	public static function sanitize_image_url( $image_url ): string {
+		// Update .tif images to be .png.
+		if ( strpos( $image_url, '.tif' ) !== false ) {
+			$image_url = str_replace( '.tif', '.png', $image_url );
+		}
+
+		// Remove query string.
+		$image_url = self::remove_query_string( $image_url );
+
+		return $image_url;
+	}
+
+	/**
+	 * Wrapper for error_log that handles both arrays and strings.
+	 *
+	 * @param string|array $log What we want logged.
+	 * @return void
+	 */
+	public static function write_log( $log ): void {
+		if ( is_array( $log ) || is_object( $log ) ) {
+			error_log( print_r( $log, true ) );
+		} else {
+			error_log( $log );
+		}
+	}
+
 }
